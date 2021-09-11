608f45e2433c5d2bb4c2e8db7d4343eda1f8ed1a
Matt Jennings <mattjennings44@gmail.com>
2021-09-11 15:32:06 -0500

    fix post sorting

--- src/lib/get-posts.js
+++ src/lib/get-posts.js
@@ -8,7 +8,9 @@ export function getPosts({ page = 1, limit } = {}) {
   const posts = Object.entries(import.meta.globEager('/posts/**/*.md'))
     .map(([, post]) => ({ metadata: post.metadata, component: post.default }))
     // sort by date
-    .sort((a, b) => (a.metadata.date < b.metadata.date ? 1 : -1))
+    .sort((a, b) => {
+      return new Date(a.metadata.date).getTime() < new Date(b.metadata.date).getTime() ? 1 : -1
+    })
     // next/previous posts
     .map((post, index, array) => {
       const next = array[index - 1]
