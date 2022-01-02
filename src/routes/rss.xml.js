50fe5206ce37392357923819dac51cd97d0ca98a
mattjennings <me@mattjennings.io>
2022-01-02 13:30:14 -0600

    fix rss feed invalidations

--- src/routes/rss.xml.js
+++ src/routes/rss.xml.js
@@ -4,19 +4,24 @@
 import { getPosts } from '$lib/get-posts'
 import { name, website } from '$lib/info'
 
-const xml = String.raw
+// update this to something more appropriate for your website
+const websiteDescription = `${name}'s blog`
+const postsUrl = `${website}/posts/`
 
+/**
+ * @type {import('@sveltejs/kit').RequestHandler}
+ */
 export async function get() {
-  const posts = getPosts()
-  const websiteDescription = `${name}'s blog`
+  // helper for vscode syntax highlighting
+  const xml = String.raw
 
-  const headers = {
-    'Cache-Control': 'max-age=0, s-maxage=3600',
-    'Content-Type': 'application/xml'
-  }
+  const posts = getPosts()
 
   return {
-    headers,
+    headers: {
+      'Cache-Control': 'max-age=0, s-maxage=3600',
+      'Content-Type': 'application/xml'
+    },
     body: xml`
     <rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
   <channel>
@@ -29,10 +34,10 @@ export async function get() {
         (post) =>
           xml`
           <item>
-            <guid>${website}/posts/${post.slug}</guid>
+            <guid>${postsUrl}${post.slug}</guid>
             <title>${post.title}</title>
             <description>${post.preview.text}</description>
-            <link>${website}/posts/${post.slug}</link>
+            <link>${postsUrl}${post.slug}</link>
             <pubDate>${new Date(post.date).toUTCString()}</pubDate>
         </item>
       `
