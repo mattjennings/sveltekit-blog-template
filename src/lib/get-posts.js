4f31bcd4efa1916ee62852836b0c5afb033f6139
Matthias Streidel <matthias.streidel@senacor.com>
2022-02-11 19:49:45 +0100

    Add custom preview as a new feature

--- src/lib/get-posts.js
+++ src/lib/get-posts.js
@@ -50,7 +50,8 @@ const posts = Object.entries(import.meta.globEager('/posts/**/*.md'))
         : undefined,
 
       // the svelte component
-      component: post.default
+      component: post.default,
+      customPreview: post.metadata.preview
     }
   })
   // parse HTML output for content metadata (preview, reading time, toc)
@@ -58,7 +59,7 @@ const posts = Object.entries(import.meta.globEager('/posts/**/*.md'))
     const parsedHtml = parse(post.component.render().html)
 
     // get the first paragaph of the post to use for the preview
-    const preview = parsedHtml.querySelector('p')
+    const preview = post.customPreview? post.customPreview : parsedHtml.querySelector('p')
 
     return {
       ...post,
