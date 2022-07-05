a73c63d3a41a5c1a7cd489eebd219ef077bce237
mattjennings <me@mattjennings.io>
2022-07-05 11:48:27 -0500

    fix stale post data when navigating between posts

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -37,49 +37,47 @@
   export let component
   export let post
 
-  const { title, date, preview, readingTime, slug, next, previous } = post
-
   // generated open-graph image for sharing on social media. Visit https://og-image.vercel.app/ to see more options.
   const ogImage = `https://og-image.vercel.app/**${encodeURIComponent(
-    title
+    post.title
   )}**?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg`
 
-  const url = `${website}/${slug}`
+  const url = `${website}/${post.slug}`
 </script>
 
 <svelte:head>
-  <title>{title}</title>
-  <meta name="description" content={preview.text} />
+  <title>{post.title}</title>
+  <meta name="description" content={post.preview.text} />
   <meta name="author" content={name} />
 
   <!-- Facebook Meta Tags -->
   <meta property="og:url" content={url} />
   <meta property="og:type" content="website" />
-  <meta property="og:title" content={title} />
-  <meta property="og:description" content={preview.text} />
+  <meta property="og:title" content={post.title} />
+  <meta property="og:description" content={post.preview.text} />
   <meta property="og:image" content={ogImage} />
 
   <!-- Twitter Meta Tags -->
   <meta name="twitter:card" content="summary_large_image" />
   <meta property="twitter:domain" content={website} />
   <meta property="twitter:url" content={url} />
-  <meta name="twitter:title" content={title} />
-  <meta name="twitter:description" content={preview.text} />
+  <meta name="twitter:title" content={post.title} />
+  <meta name="twitter:description" content={post.preview.text} />
   <meta name="twitter:image" content={ogImage} />
 </svelte:head>
 
 <article class="relative">
   <h1 class="!mt-0 !mb-2">
     <a class="!font-medium" href={$page.url.pathname}>
-      {title}
+      {post.title}
     </a>
   </h1>
   <div class="opacity-70">
-    <time datetime={new Date(parseISO(date)).toISOString()}
-      >{format(new Date(parseISO(date)), 'MMMM d, yyyy')}</time
+    <time datetime={new Date(parseISO(post.date)).toISOString()}
+      >{format(new Date(parseISO(post.date)), 'MMMM d, yyyy')}</time
     >
     •
-    <span>{readingTime}</span>
+    <span>{post.readingTime}</span>
   </div>
 
   <div class="relative">
@@ -107,24 +105,24 @@
 </div>
 
 <!-- next/previous posts -->
-{#if previous || next}
+{#if post.previous || post.next}
   <hr />
   <div class="grid gap-8 grid-cols-1 sm:grid-cols-2">
-    {#if previous}
+    {#if post.previous}
       <div class="flex flex-col">
         <h6 class="not-prose post-preview-label">Previous Post</h6>
         <div class="flex-1 post-preview">
-          <PostPreview post={previous} small />
+          <PostPreview post={post.previous} small />
         </div>
       </div>
     {:else}
       <div />
     {/if}
-    {#if next}
+    {#if post.next}
       <div class="flex flex-col">
         <h6 class="not-prose post-preview-label flex justify-end">Next Post</h6>
         <div class="flex-1 post-preview">
-          <PostPreview post={next} small />
+          <PostPreview post={post.next} small />
         </div>
       </div>
     {/if}
