3ec10c0abf37b91a6bab0d3d0e859e0ae3b69451
mattjennings <me@mattjennings.io>
2022-08-16 19:46:52 -0500

    update post

--- src/routes/posts/[slug]/+page.js
+++ src/routes/posts/[slug]/+page.js
@@ -1,4 +1,7 @@
 /**
+ * Dynamically loads the svelte component for the post (only possible in +page.js)
+ * and pass on the data from +page.server.js
+ *
  * @type {import('@sveltejs/kit').PageLoad}
  */
 export async function load({ data }) {
