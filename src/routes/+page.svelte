21c38c7735242882ab85517475a682fb6ff5c1a9
Matt Jennings <me@mattjennings.io>
2022-10-09 17:39:28 -0500

    add heading to posts page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,9 +31,11 @@
     </div>
   </section>
   <section class="w-full">
-    <div class="flex items-baseline justify-between gap-4 mb-8">
-      <h2 class="text-xl opacity-75">Recent Articles</h2>
-      <a href="/posts" class="flex items-center gap-1 text-xs font-medium text-teal-500"
+    <div class="flex items-center justify-between gap-4 mb-8">
+      <h2 class="text-sm font-medium sm:text-base text-zinc-400 dark:text-zinc-500">
+        Recently Published
+      </h2>
+      <a href="/posts" class="flex items-center gap-1 text-sm font-medium text-teal-500"
         >View All <ArrowRightIcon class="w-4 h-4" /></a
       >
     </div>
--- src/routes/posts/[...page]/+page.svelte
+++ src/routes/posts/[...page]/+page.svelte
@@ -16,7 +16,16 @@
 </svelte:head>
 
 <div class="flex flex-col flex-grow">
-  <PostsList posts={data.posts} />
+  <header class="pt-4">
+    <h1 class="text-4xl font-bold tracking-tight sm:text-5xl">
+      Writing on tech, music, and whatever else I feel like
+    </h1>
+    <p class="mt-6">All of my written content collected in one place</p>
+  </header>
+
+  <div class="mt-16 sm:mt-20">
+    <PostsList posts={data.posts} />
+  </div>
 
   <!-- pagination -->
   <div class="flex items-center justify-between pt-16 pb-8">
