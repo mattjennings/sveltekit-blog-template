2e66f99bf7647e3e2889eff5561dd6597190c44f
Matt Jennings <me@mattjennings.io>
2022-10-09 20:34:56 -0500

    better document info.js

--- src/lib/info.js
+++ src/lib/info.js
@@ -2,13 +2,15 @@
 // If you'd rather hardcode these values into your pages,
 // feel free to remove this file.
 
-// name
+// URL of your website
+export const website = 'https://sveltekit-blog-template.vercel.app'
+
 export const firstName = 'Jordan'
 export const lastName = 'Doe'
 export const name = `${firstName} ${lastName}`
 
+// Used for the landing page & footer of posts
 export const avatar = 'https://api.lorem.space/image/face?w=250&h=250'
-
 export const bio = `\
 I’m ${firstName}, a software engineer based in Canada. Lorem ipsum dolor sit amet, consectetur
 adipiscing elit. Phasellus accumsan quam at eros malesuada, id luctus lectus efficitur. Nulla
@@ -16,11 +18,10 @@ sit amet arcu mi. Duis quis mauris malesuada, posuere orci et, pulvinar nisl. Na
 vehicula sapien vitae sagittis.
 `
 
-// socials - replace with your usernames
+// Your social usernames, empty strings won't be used.
+// For sake of the live demo, I'm using `username/repo` for github,
+// but you can replace with just your username.
+export const github = 'mattjennings/sveltekit-blog-template'
 export const twitter = ''
-export const github = 'mattjennings/sveltekit-blog-template' // change this to be just your username
 export const linkedin = ''
 export const instagram = ''
-
-// url of your website
-export const website = 'https://sveltekit-blog-template.vercel.app'
